c0899481ffd387a04f7598bb3d3f747884406b85
alishahidi1 <ali.shahidi@uwaterloo.ca>
2024-01-25 15:48:12 -0500

    updated

---
+++ script.js
@@ -0,0 +1,17 @@
+let Name = "Ali";
+console.log(Name);
+
+const last_name = "Shhdi";
+
+let age = 33;
+
+// Defining an object
+let person = {
+    Name : "Ali",
+    age : 33,
+    admin : true
+}
+
+
+
+
