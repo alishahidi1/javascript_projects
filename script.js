d92247e6ad0e65ccb9bfbc3edc75788fdb166392
alishahidi1 <ali.shahidi@uwaterloo.ca>
2024-01-25 18:43:36 -0500

    updated

--- script.js
+++ script.js
@@ -11,6 +11,23 @@ let person = {
     age : 33,
     admin : true
 }
+console.log(person);
+console.log(person.age);
+
+// Array definition:
+let users = ["Pedram", "Behrooz"]
+console.log(users)
+
+// function definition:
+function greet(nme){
+    console.log('Hello' + nme +"!!!")
+}
+
+// if conidtions:
+let authenticated = true;
+//ternery
+let bottonText = authenticated === true ? "Dashboard" : "Login";
+
 
 
 
